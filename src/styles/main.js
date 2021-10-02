2952cdebab47e005e81d8fd9eb02ee2562f22428
marcusharding <marcus.harding@kaldorgroup.com>
2021-10-02 14:36:53 +0100

    Get a basic app screen in place for now

---
+++ src/styles/main.js
@@ -0,0 +1,13 @@
+import {Platform, StyleSheet} from 'react-native';
+
+const appTheme = StyleSheet.create({
+	colors: {
+		primary: '',
+		background: '#121212',
+		card: '#111111',
+		text: '#fff',
+		notification: 'rgb(255, 69, 58)',
+	},
+});
+
+export {appTheme};
