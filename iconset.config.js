027bf954ca5b7ec63cf6fb10566c7ba1f3f64e25
marcusharding <marcus.harding@kaldorgroup.com>
2021-10-02 15:18:16 +0100

    App icons

---
+++ iconset.config.js
@@ -0,0 +1,7 @@
+/*iconset.config.js*/
+module.exports = {
+	imagePath: './assets/icon.png',
+
+	adaptiveIconBackground: './assets/icon_background.png',
+	adaptiveIconForeground: './assets/icon.png',
+};
