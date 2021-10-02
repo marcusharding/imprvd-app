4829f191f24be7dc9f2200fc90b263177cf05782
marcusharding <marcus.harding@kaldorgroup.com>
2021-10-02 16:50:00 +0100

    Start rebuilding app (React does not like the nested tab navigator)

--- App.js
+++ App.js
@@ -1,26 +1,72 @@
-/**
- * @format
- * @flow strict-local
- */
-
 // React
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// Firebase
+import auth from '@react-native-firebase/auth';
 
 // Styles
 import {appTheme} from './src/styles/main';
 
+//Screens
+import TestScreen from './testScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Home = () => {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name="TestScreen" component={TestScreen} />
+		</Tab.Navigator>
+	);
+};
+
 const App = () => {
-	React.useEffect(() => {
+	const [initializing, setInitializing] = useState(true);
+	const [user, setUser] = useState();
+
+	const onAuthStateChanged = user => {
+		setUser(user);
+		if (initializing) {
+			setInitializing(false);
+		}
+	};
+
+	useEffect(() => {
 		SplashScreen.hide();
+		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+		return subscriber; // unsubscribe on unmount
 	});
+
+	if (initializing) {
+		return null;
+	}
+
+	if (!user) {
+		return (
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen
+						name="Home"
+						component={Home}
+						options={{headerShown: false}}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		);
+	}
+
 	return (
 		<SafeAreaView style={appTheme}>
 			<StatusBar />
 			<ScrollView contentInsetAdjustmentBehavior="automatic">
 				<View>
-					<Text>This is the app rebuild</Text>
+					<Text>Welcome {user.email}</Text>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
