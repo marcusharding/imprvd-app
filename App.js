2592a4ff92c87e03423ee331e1218b5533eac9a6
marcusharding <marcus.harding@kaldorgroup.com>
2021-10-02 16:01:09 +0100

    Splashscreen for android

--- App.js
+++ App.js
@@ -6,11 +6,15 @@
 // React
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 // Styles
 import {appTheme} from './src/styles/main';
 
 const App = () => {
+	React.useEffect(() => {
+		SplashScreen.hide();
+	});
 	return (
 		<SafeAreaView style={appTheme}>
 			<StatusBar />
