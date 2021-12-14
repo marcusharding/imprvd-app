ad7e9926b2d0eb670ab6e9bae7b57f3e990c3dd4
marcusharding <marcus.harding@kaldorgroup.com>
2021-12-14 22:09:58 +0000

    IMPRVD-175 - Moving some functionality in js, now whats wrong with the picker

--- src/components/modalScreens/addNewBenchmark.js
+++ src/components/modalScreens/addNewBenchmark.js
@@ -1,5 +1,5 @@
 // React
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, ScrollView, SafeAreaView, Alert} from 'react-native';
 
 // Partials
@@ -11,160 +11,113 @@ import AddNewBenchmarkFields from './addNewBenchmarkFields';
 // Styles
 import {typography, spacing} from '../../styles/main';
 
-class AddNewBenchmark extends Component {
-	constructor() {
-		super();
+// Scripts
+import {
+	fetchBenchmarksList,
+	fetchBenchmarkTags,
+	fetchBenchmarkFields,
+} from '../../scripts/benchmarks';
 
-		this.state = {
-			subTitle: '',
-			isLoading: true,
-			benchmarksList: [],
-			benchmarkFields: [],
-			selectedBenchmark: null,
-		};
+const AddNewBenchmark = ({navigation}) => {
+	const [subTitle, setSubTitle] = useState('');
+	const [isLoading, setLoading] = useState(false);
+	const [benchmarksList, setBenchmarksList] = useState([]);
+	const [benchmarkFields, setBenchmarkFields] = useState([]);
+	const [selectedBenchmark, setSelectedBenchmark] = useState('gymnastics');
 
-		this.setSelectedBenchmark = this.setSelectedBenchmark.bind(this);
-	}
+	const getBenchmarkFields = async tagIds => {
+		setBenchmarkFields([]);
+		const fields = await fetchBenchmarkFields(tagIds);
+		if (fields) {
+			setBenchmarkFields(fields);
+		}
+	};
 
-	fetchData() {
-		const {benchmarksList} = this.state;
+	const getBenchmarkTags = async () => {
+		setLoading(true);
+		const tags = await fetchBenchmarkTags(
+			`https://contentmanagement.getimprvd.app/wp-json/wp/v2/app_benchmarks?slug=${selectedBenchmark}`,
+		);
+
+		if (tags) {
+			getBenchmarkFields(tags);
+		}
+	};
+
+	// const _setSelectedBenchmark = callback => {
+	// 	// setSelectedBenchmark(callback(selectedBenchmark));
+	// 	// fetchBenchmarkTags(callback);
+	// 	console.log(callback(selectedBenchmark));
+	// };
+
+	const getBenchmarksList = async () => {
+		const list = await fetchBenchmarksList(
+			'https://contentmanagement.getimprvd.app/wp-json/wp/v2/app_benchmarks',
+		);
+
+		if (list) {
+			setBenchmarksList(list);
+			setLoading(false);
+		}
+	};
+
+	const fetchScreenData = () => {
 		fetch(
 			'https://contentmanagement.getimprvd.app/wp-json/wp/v2/app_screens?slug=add-new-benchmark',
 		)
 			.then(response => response.json())
 			.then(json => {
 				const data = json[0].acf;
-
-				this.setState({
-					subTitle: data.screen_subtitle,
-					isLoading: false,
-				});
+				setSubTitle(data.screen_subtitle);
 			})
-			.then(
-				fetch(
-					'https://contentmanagement.getimprvd.app/wp-json/wp/v2/app_benchmarks',
-				)
-					.then(response => response.json())
-					.then(json => {
-						json.map(benchmark => {
-							benchmarksList.push({
-								label: benchmark.title.rendered,
-								value: benchmark.slug,
-							});
-							this.setState({
-								benchmarksList: benchmarksList,
-							});
-						});
-					}),
-			)
+			.then(getBenchmarksList())
 			.catch(error => {
 				console.log(error);
 				Alert.alert('Error:', error.message);
 			});
-	}
-
-	setSelectedBenchmark(callback) {
-		const {selectedBenchmark} = this.state;
-
-		this.setState(state => ({
-			selectedBenchmark: callback(state.selectedBenchmark),
-		}));
-
-		this.fetchBenchmarkTags(callback(selectedBenchmark));
-	}
-
-	fetchBenchmarkTags(selectedBenchmark) {
-		this.setState({
-			isLoading: true,
-			benchmarkFields: [],
-		});
-
-		fetch(
-			`https://contentmanagement.getimprvd.app/wp-json/wp/v2/app_benchmarks?slug=${selectedBenchmark}`,
-		)
-			.then(response => response.json())
-			.then(json => {
-				this.fetchBenchmarkFields(json[0].tags);
-			})
-			.catch(error => {
-				console.log(error);
-				Alert.alert('Error:', error.message);
-				this.setState({
-					isLoading: false,
-				});
-			});
-	}
-
-	fetchBenchmarkFields = async tagIds => {
-		const {benchmarkFields} = this.state;
-		const count = tagIds.length;
-		let counter = 0;
-
-		for (const id of tagIds) {
-			const response = await fetch(
-				`https://contentmanagement.getimprvd.app/wp-json/wp/v2/tags?include=${id}`,
-			);
-			const json = await response.json();
-			benchmarkFields.push({
-				name: json[0].name,
-				slug: json[0].slug,
-			});
-			counter = counter + 1;
-			if (counter === count) {
-				this.setState({
-					benchmarkFields: benchmarkFields,
-					isLoading: false,
-				});
-			}
-		}
 	};
 
-	componentDidMount() {
-		this.fetchData();
+	const fetchData = () => {
+		setLoading(true);
+		fetchScreenData();
+		getBenchmarkTags();
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	if (isLoading) {
+		return <PreLoader />;
 	}
 
-	render() {
-		const {
-			isLoading,
-			subTitle,
-			benchmarksList,
-			selectedBenchmark,
-			benchmarkFields,
-		} = this.state;
-		const {navigation} = this.props;
-
-		if (isLoading) {
-			return <PreLoader />;
-		}
-
-		return (
-			<SafeAreaView style={spacing.flex1}>
-				<GoBackIcon navigation={navigation} />
-				<Text
-					style={[
-						typography.subHeading,
-						spacing.marginBottom20,
-						spacing.marginTop20,
-					]}>
-					{subTitle}
-				</Text>
-				<View style={spacing.marginBottom20}>
-					<DropDownSelector
-						list={benchmarksList}
-						setSelectedBenchmark={this.setSelectedBenchmark}
-						selectedBenchmark={selectedBenchmark}
-					/>
-				</View>
-				<ScrollView>
-					<AddNewBenchmarkFields
-						benchmarkFields={benchmarkFields}
-						selectedBenchmark={selectedBenchmark}
-						navigation={navigation}
-					/>
-				</ScrollView>
-			</SafeAreaView>
-		);
-	}
-}
+	return (
+		<SafeAreaView style={spacing.flex1}>
+			<GoBackIcon navigation={navigation} />
+			<Text
+				style={[
+					typography.subHeading,
+					spacing.marginBottom20,
+					spacing.marginTop20,
+				]}>
+				{subTitle}
+			</Text>
+			<View style={spacing.marginBottom20}>
+				{/* <DropDownSelector
+					list={benchmarksList}
+					setSelectedBenchmark={_setSelectedBenchmark}
+					selectedBenchmark={selectedBenchmark}
+				/> */}
+			</View>
+			<ScrollView>
+				<AddNewBenchmarkFields
+					benchmarkFields={benchmarkFields}
+					selectedBenchmark={selectedBenchmark}
+					navigation={navigation}
+				/>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
 
 export default AddNewBenchmark;
--- src/scripts/benchmarks.js
+++ src/scripts/benchmarks.js
@@ -1,15 +1,45 @@
 export const fetchBenchmarksList = async url => {
 	const response = await fetch(url);
 	const json = await response.json();
-	const benchmarksList = [];
+	const list = [];
 
 	if (json) {
 		json.map(benchmark => {
-			benchmarksList.push({
+			list.push({
 				slug: benchmark.slug,
 				label: benchmark.title.rendered,
 			});
 		});
-		return benchmarksList;
+		return list;
+	}
+};
+
+export const fetchBenchmarkTags = async url => {
+	const response = await fetch(url);
+	const json = await response.json();
+
+	if (json) {
+		return json[0].tags;
+	}
+};
+
+export const fetchBenchmarkFields = async tagIds => {
+	const count = tagIds.length;
+	let counter = 0;
+	const fields = [];
+
+	for (const id of tagIds) {
+		const response = await fetch(
+			`https://contentmanagement.getimprvd.app/wp-json/wp/v2/tags?include=${id}`,
+		);
+		const json = await response.json();
+		fields.push({
+			name: json[0].name,
+			slug: json[0].slug,
+		});
+		counter = counter + 1;
+		if (counter === count) {
+			return fields;
+		}
 	}
 };
