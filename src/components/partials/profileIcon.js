be72a6f1a8b33082be053618b54f9d4f40cf881b
marcusharding <marcus.harding@kaldorgroup.com>
2021-12-22 15:19:22 +0000

    Better handling of returning appropriate icon for profile icon

--- src/components/partials/profileIcon.js
+++ src/components/partials/profileIcon.js
@@ -12,28 +12,10 @@ import auth from '@react-native-firebase/auth';
 
 const ProfileIcon = ({navigation}) => {
 	const {photoURL} = auth().currentUser;
-	return (
-		<View>
-			{!photoURL && (
-				<TouchableOpacity
-					activeOpacity={0.8}
-					onPress={() =>
-						navigation.dispatch(
-							CommonActions.navigate({
-								name: 'ProfileScreen',
-							}),
-						)
-					}>
-					<MaterialCommunityIcons
-						name={'account-circle'}
-						color={'#808080'}
-						size={35}
-						style={baseStyles.profileIcon}
-					/>
-				</TouchableOpacity>
-			)}
 
-			{photoURL && (
+	if (photoURL) {
+		return (
+			<View>
 				<TouchableOpacity
 					activeOpacity={0.8}
 					onPress={() =>
@@ -45,7 +27,28 @@ const ProfileIcon = ({navigation}) => {
 					}>
 					<Image style={baseStyles.profileIcon} source={{uri: photoURL}} />
 				</TouchableOpacity>
-			)}
+			</View>
+		);
+	}
+
+	return (
+		<View>
+			<TouchableOpacity
+				activeOpacity={0.8}
+				onPress={() =>
+					navigation.dispatch(
+						CommonActions.navigate({
+							name: 'ProfileScreen',
+						}),
+					)
+				}>
+				<MaterialCommunityIcons
+					name={'account-circle'}
+					color={'#808080'}
+					size={35}
+					style={baseStyles.profileIcon}
+				/>
+			</TouchableOpacity>
 		</View>
 	);
 };
